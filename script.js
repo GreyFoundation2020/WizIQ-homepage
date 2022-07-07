e3b6b2c8f8df125a4302dd63a2ea007058f654ef
GreyFoundation2020 <gregoryudofa@gmail.com>
2022-07-07 10:32:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// // const images = [
+// //     './images/21.png',
+// //     './images/22.png',
+// //     "/images/23.png",
+// //     "/images/24.png",
+// // ]
+
+
+
+// // const handleClick = (n) => [
+// //     document.getElementById("myImg").src = images[n],
